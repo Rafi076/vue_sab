5987280ba3fca2f2506998fb6967e90a4b0fe696
Rafi076 <mohiuddinrafi076@gmail.com>
2025-10-16 17:25:11 +0600

    Reusable component, pass data with props & emit events

---
+++ component_props/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Reusable Button Example</h1>
+    
+    <!-- Using button with default color -->
+    <ButtonComponent label="Click Me" @clicked="onButtonClick" />
+
+    <!-- Using button with custom color -->
+    <ButtonComponent label="Delete" color="red" @clicked="onDeleteClick" />
+  </div>
+</template>
+
+<script>
+import ButtonComponent from './components/ButtonComponent.vue';
+
+export default {
+  name: 'App',
+  components: { ButtonComponent },
+  methods: {
+    onButtonClick() {
+      alert('Button clicked!');
+    },
+    onDeleteClick() {
+      alert('Delete button clicked!');
+    }
+  }
+}
+</script>
---
+++ component_props/src/components/ButtonComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <button @click="handleClick" :style="buttonStyle">
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonComponent',
+  props: {
+    label: { // text displayed on the button
+      type: String,
+      required: true
+    },
+    color: { // optional prop to change button color
+      type: String,
+      default: 'blue'
+    }
+  },
+  computed: {
+    buttonStyle() {
+      return {
+        backgroundColor: this.color,
+        color: 'white',
+        padding: '10px 20px',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer'
+        
+      };
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$emit('clicked'); // emit event to parent
+    }
+  }
+}
+</script>
